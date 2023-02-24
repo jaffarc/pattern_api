0769f67e8aaaa337fcae1c90212e13df38ec371f
Jaffar Cardoso <jaffar.cardoso@gmail.com>
2023-02-24 17:45:53 -0300

    remove cod nao usado

--- src/server.js
+++ src/server.js
@@ -59,33 +59,6 @@ app.use((err, req, res, next) => {
 });
 
 
-app.use((req, res, next) => {
-  let requestTime = Date.now();
-  res.on('finish', () => {
-    console.log({
-      all:function (chunk, encoding, callback) {
-        logMessage(options.debug, 'response write', 'append chunk=' + chunk);
-        if (
-          !resBodyBufLimitedExceeded &&
-          totalChunkLength(resBodyBuf, chunk) < options.responseMaxBodySize
-        ) {
-          resBodyBuf = appendChunk(resBodyBuf, chunk);
-        } else {
-          resBodyBufLimitedExceeded = true;
-        }
-        res._mo_write(chunk, encoding, callback);
-      },
-      url: req.path,
-      method: req.method,
-      user: req.user ? req.user._id : '',
-      body: req.body,
-      responseTime: (Date.now() - requestTime) / 1000, // convert to seconds
-      day: new Date(requestTime).getUTCDay(),
-      hour: `${new Date(requestTime).getHours()}:${new Date(requestTime).getUTCMinutes()}`
-    });
-  });
-  next();
-});
 app.use(require("./api/router/registerRouter"));
 
 module.exports = app;
